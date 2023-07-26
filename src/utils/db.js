b80f9829ad4d18c4719c7d8fe300188f8c41704f
Jviguy <jviguytwo2@gmail.com>
2023-07-26 13:05:14 -0400

    fixes

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
-export default async () => {
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
     } catch(err) {
         throw new Error(process.env.MONGODB_URI)
     }
 }
+
+export default connect;
