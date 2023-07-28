88418ebb97ec3fb8ef5f2bbc8bd3269030ae98ac
Jviguy <jviguytwo2@gmail.com>
2023-07-28 01:48:09 -0400

    propery handling of loading navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import styles from "./navbar.module.css"
 import {useSession, signOut} from "next-auth/react";
 import {usePathname} from "next/navigation";
+import {Suspense} from "react";
 
 const links = [
     "Blog", "Portfolio", "Contact"
@@ -13,6 +14,17 @@ const links = [
 const Navbar = () => {
     const {data,status} =  useSession()
     const path = usePathname();
+    if (status === "loading") return (<Suspense>
+        <div className={styles.container}>
+            <Link className={styles.emoji} href="/">🏠</Link>
+            <div className={styles.links}>
+                {links.map((link, index) => (
+                    <Link className={styles.link}  key={index} href={link.toLowerCase()}>{link}</Link>
+                ))}
+                <button className={styles.login}>Sign</button>
+            </div>
+        </div>
+    </Suspense>);
     return (
         <div className={styles.container}>
             <Link className={styles.emoji} href="/">🏠</Link>
