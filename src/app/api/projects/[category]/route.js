9e3e9c81ec42c38edb80254212bc5c85e4cf8982
Jviguy <jviguytwo2@gmail.com>
2023-08-06 13:45:47 -0400

    finish category page

--- src/app/api/projects/[category]/route.js
+++ src/app/api/projects/[category]/route.js
@@ -6,7 +6,6 @@ export const GET = async (req, {params}) => {
     try {
         await connect();
         const posts = await Project.find({type: params.category});
-        console.log("POSTS: " + posts);
         return new NextResponse(JSON.stringify(posts), {status: 200});
     } catch (err) {
         return new NextResponse("Database error: " + err.toString(), {status: 500});
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -3,24 +3,24 @@ import {NotFound} from "next/dist/client/components/error";
 import styles from "./page.module.css"
 
 async function retrieveProjects(category) {
-	const response = await fetch(`https://jviguy.vercel.app/api/projects/${category}`)
+	const response = await fetch(`https://jviguy.vercel.app/api/projects/${category}`, { next: { revalidate: 10 } })
 	if (!response.ok) return NotFound();
 	return response.json();
 }
 
 const Page = async ({params}) => {
-	const data = await retrieveProjects(params.category);
+	let data = await retrieveProjects(params.category)
 	if (React.isValidElement(data)) return data
 	return (
 		<div className={styles.container}>
-			{data.map(project=>{
-				return (
+			{data.map(project=>(
 					<div key={project.title} className={styles.project}>
 						<h1 className={styles.title}>{project.title}</h1>
 						<p>{project.description}</p>
+						<button className={styles.viewButton}>See More</button>
 					</div>
 				)
-			})}
+			)}
 		</div>
 	)
 }
