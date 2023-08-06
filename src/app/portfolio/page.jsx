c668c84c881f796c6b9ccfd78173bb267970c4d2
Jviguy <jviguytwo2@gmail.com>
2023-08-06 08:05:20 -0400

    start work on portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from "./page.module.css"
+import Link from "next/link";
 const Page = () => (
-    <div>
-        PORTFOLIO
+    <div className={styles.container}>
+        <Link href="/portfolio/personal">
+            <div className={styles.categoryContainer}>
+                <h1 className={styles.title}>Personal Projects</h1>
+            </div>
+        </Link>
+        <Link href="/portfolio/professional">
+            <div className={styles.categoryContainer}>
+                <h1 className={styles.title}>Professional Projects</h1>
+            </div>
+        </Link>
     </div>
 );
 
